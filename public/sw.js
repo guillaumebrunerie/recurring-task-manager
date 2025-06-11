0d72721f7af7a33b768ee55f3bf89c68c74ddbb5
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2025-06-11 23:00:45 +0200

    try changes to service worker

--- public/sw.js
+++ public/sw.js
@@ -3,13 +3,7 @@ self.addEventListener("push", function (event) {
 		const data = event.data.json();
 		const options = {
 			body: data.body,
-			icon: data.icon || "/icon.png",
-			badge: "/badge.png",
-			vibrate: [100, 50, 100],
-			data: {
-				dateOfArrival: Date.now(),
-				primaryKey: "2",
-			},
+			badge: "/icon.png",
 		};
 		event.waitUntil(
 			self.registration.showNotification(data.title, options),
@@ -18,7 +12,8 @@ self.addEventListener("push", function (event) {
 });
 
 self.addEventListener("notificationclick", function (event) {
-	console.log("Notification click received.");
 	event.notification.close();
-	event.waitUntil(clients.openWindow("<https://your-website.com>"));
+	event.waitUntil(
+		clients.openWindow("https://project-happy-home.netlify.app/"),
+	);
 });
