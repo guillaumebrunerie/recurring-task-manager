a4d1d56dedd30e600182eea92483dadf13c261cd
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2025-06-11 21:23:33 +0200

    fix sw.ts

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,24 @@
-"use strict";
 self.addEventListener("push", function (event) {
-    if (event.data) {
-        var data = event.data.json();
-        var options = {
-            body: data.body,
-            icon: data.icon || "/icon.png",
-            badge: "/badge.png",
-            vibrate: [100, 50, 100],
-            data: {
-                dateOfArrival: Date.now(),
-                primaryKey: "2",
-            },
-        };
-        event.waitUntil(self.registration.showNotification(data.title, options));
-    }
+	if (event.data) {
+		const data = event.data.json();
+		const options = {
+			body: data.body,
+			icon: data.icon || "/icon.png",
+			badge: "/badge.png",
+			vibrate: [100, 50, 100],
+			data: {
+				dateOfArrival: Date.now(),
+				primaryKey: "2",
+			},
+		};
+		event.waitUntil(
+			self.registration.showNotification(data.title, options),
+		);
+	}
 });
+
 self.addEventListener("notificationclick", function (event) {
-    console.log("Notification click received.");
-    event.notification.close();
-    event.waitUntil(clients.openWindow("<https://your-website.com>"));
+	console.log("Notification click received.");
+	event.notification.close();
+	event.waitUntil(clients.openWindow("<https://your-website.com>"));
 });
