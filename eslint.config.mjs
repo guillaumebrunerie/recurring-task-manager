df9962b71e09d87cd80729c6b947bd1cd2e5c11e
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2025-08-25 21:52:29 +0200

    update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import { globalIgnores } from "eslint/config";
+import convexPlugin from "@convex-dev/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+	...convexPlugin.configs.recommended,
 	...compat.config({
 		extends: ["next/core-web-vitals", "next/typescript"],
 		rules: {
