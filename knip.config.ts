de21c556f582e032c697d5201c8dfd5ce686eb54
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2026-01-05 20:08:25 +0100

    fix setup

--- knip.config.ts
+++ knip.config.ts
@@ -1,13 +1,9 @@
 import type { KnipConfig } from "knip";
 
 export default {
-	entry: [
-		"src/service-worker/sw.ts", // Service worker
-	],
 	ignore: [
 		"src/convex/auth.ts", // Used by Convex
 		"src/convex/auth.config.ts", // Used by Convex
 		"src/convex/_generated/*", // Generated code
 	],
-	ignoreDependencies: [],
 } satisfies KnipConfig;
