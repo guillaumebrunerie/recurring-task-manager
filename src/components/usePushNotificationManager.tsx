ea7e0ec5ce69b9d5d3addd503683a6509803cdc5
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2026-03-22 00:41:14 +0100

    try stuff

--- src/components/usePushNotificationManager.tsx
+++ src/components/usePushNotificationManager.tsx
@@ -10,7 +10,7 @@ import {
 
 import { api } from "@/convex/_generated/api";
 
-import { publicEnv } from "@/shared/publicEnv";
+// import { publicEnv } from "@/shared/publicEnv";
 
 const urlBase64ToUint8Array = (base64String: string) => {
 	const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
@@ -83,7 +83,7 @@ export const usePushNotificationManager = (): NotificationsProps => {
 			const sub = await registration.pushManager.subscribe({
 				userVisibleOnly: true,
 				applicationServerKey: urlBase64ToUint8Array(
-					publicEnv.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+					process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
 				),
 			});
 			setSubscription(sub);
