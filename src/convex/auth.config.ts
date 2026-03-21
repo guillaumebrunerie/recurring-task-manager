418a502843a61c9cca4d155b0edee78ab03571a9
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2026-03-21 23:49:06 +0100

    try other env variable for auth

--- src/convex/auth.config.ts
+++ src/convex/auth.config.ts
@@ -2,5 +2,10 @@ import { authEnv } from "./authEnv";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	providers: [{ domain: authEnv.CONVEX_SITE_URL, applicationID: "convex" }],
+	providers: [
+		{
+			domain: authEnv.NEXT_PUBLIC_CONVEX_SITE_URL,
+			applicationID: "convex",
+		},
+	],
 };
--- src/convex/authEnv.ts
+++ src/convex/authEnv.ts
@@ -1,6 +1,6 @@
 import { type } from "arktype";
 
-const authEnvType = type({ CONVEX_SITE_URL: "string" });
+const authEnvType = type({ NEXT_PUBLIC_CONVEX_SITE_URL: "string" });
 
 // oxlint-disable-next-line node/no-process-env
 export const authEnv = authEnvType.assert(process.env);
